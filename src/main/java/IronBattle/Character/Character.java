a4dc500e9a647f847cabcf0dd5a4ffa465795756
halev <halev1992@mail.ru>
2025-07-22 15:24:31 +0200

    fix: refactor Character class fields namings

--- src/main/java/IronBattle/Character/Character.java
+++ src/main/java/IronBattle/Character/Character.java
@@ -1,44 +1,51 @@
 package IronBattle.Character;
 
-import java.util.Random;
+import java.util.UUID;
 
 public abstract class Character {
-    private final String idChar;  // ID como "Char-00001"
-    private String nameChar;
-    private int health;
-    private boolean alive;
+    private final String id;
+    private String name;
+    private int hp;
+    private boolean isAlive;
 
-    private static final int MIN_ID = 1;
-    private static final int MAX_ID = 10000;
-    private static final Random rand = new Random();
-
-    // Constructor
-    public Character(String nameChar, int health, boolean alive) {
-        this.idChar = "Char-" + String.format("%04d", rand.nextInt(MIN_ID, MAX_ID)); // Java 17+
-        this.nameChar = nameChar;
-        this.health = health;
-        this.alive = alive;
+    public Character(String name, int hp) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.hp = hp;
+        this.isAlive = true;
     }
 
     // Getters
-    public String getIdChar() {
-        return idChar;  // Ejemplo: "Char-00427"
+    public String getId() {
+        return id;
     }
 
-    public String getNameChar() { return nameChar; }
-    public int getHealth() { return health; }
-    public boolean isAlive() { return alive; }
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
 
     // Setters
-    public void setNameChar(String nameChar) { this.nameChar = nameChar; }
-
-    public void setHealth(int health) {
-        if (health <= 0) {
-            this.health = 0;
-            setAlive(false);
-        } else
-            this.health = health;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setAlive(boolean alive) { this.alive = alive; }
+    public void setHp(int hp) {
+        if (hp <= 0) {
+            this.hp = 0;
+            setAlive(false);
+        } else
+            this.hp = hp;
+    }
+
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
 }
